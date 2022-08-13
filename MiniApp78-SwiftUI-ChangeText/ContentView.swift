5388eb70870cc4450df3c6f67a363e1b1f3e6f02
Kota <maedakoutaa@gmail.com>
2022-08-13 19:48:15 +0900

    Initial Commit

---
+++ MiniApp78-SwiftUI-ChangeText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MiniApp78-SwiftUI-ChangeText
+//
+//  Created by 前田航汰 on 2022/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp78-SwiftUI-ChangeText/MiniApp78_SwiftUI_ChangeTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp78_SwiftUI_ChangeTextApp.swift
+//  MiniApp78-SwiftUI-ChangeText
+//
+//  Created by 前田航汰 on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp78_SwiftUI_ChangeTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
